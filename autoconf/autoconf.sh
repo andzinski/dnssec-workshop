5ebbcd2e5927b4402c5836f4155f04d8cc937451
Maciej Andzinski <m.andzinski@gmail.com>
2017-03-03 15:48:20 +0100

    Wsparcie dla wielu domen

--- autoconf/autoconf.sh
+++ autoconf/autoconf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 MYIP=`ifconfig eth0 | perl -ne 'print $1 if m/inet addr:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/'`
 MYHOSTNAME=`hostname`
-MYDOMAIN=`dig ${MYHOSTNAME}.szkolenie.dnssec.pl txt +short | tr -d '"'`
+MYDOMAINS=`dig ${MYHOSTNAME}.szkolenie.dnssec.pl txt +short | tr -d '"' | tr "\n" " "`
 
 echo -e "\n\tIP:\t$MYIP"
-echo -e "\tDOMENA:\t$MYDOMAIN\n"
+echo -e "\tDOMENY:\t$MYDOMAINS\n"
 
 read -p "Przeprowadzić autokonfigurację? (t/n)" choice
 case "$choice" in 
@@ -25,28 +25,48 @@ cp named.conf.options /etc/bind/named.conf.options
 
 echo 'include "/etc/bind/named.conf.zones";' >> /etc/bind/named.conf
 [ $? -ne 0 ] && exit 1
-cp zone-conf-tpl /etc/bind/named.conf.zones
+
+touch /etc/bind/named.conf.zones
 [ $? -ne 0 ] && exit 1
+
 chgrp bind /etc/bind/named.conf.zones 
 [ $? -ne 0 ] && exit 1
-sed "s/_MYDOMAIN_/$MYDOMAIN/g" -i /etc/bind/named.conf.zones
-[ $? -ne 0 ] && exit 1
+
 
 mkdir /etc/bind/zones
 [ $? -ne 0 ] && exit 1
 chown -R bind:bind /etc/bind/zones
 [ $? -ne 0 ] && exit 1
-cp zone-tpl /etc/bind/zones/$MYDOMAIN
-[ $? -ne 0 ] && exit 1
-sed "s/_MYDOMAIN_/$MYDOMAIN/g" -i /etc/bind/zones/$MYDOMAIN
-[ $? -ne 0 ] && exit 1
-sed "s/_MYIP_/$MYIP/g" -i /etc/bind/zones/$MYDOMAIN
-[ $? -ne 0 ] && exit 1
+
+
+
+for MYDOMAIN in $MYDOMAINS
+do
+	cat zone-conf-tpl >> /etc/bind/named.conf.zones
+	[ $? -ne 0 ] && exit 1
+
+	sed "s/_MYDOMAIN_/$MYDOMAIN/g" -i /etc/bind/named.conf.zones
+	[ $? -ne 0 ] && exit 1
+
+	cp zone-tpl /etc/bind/zones/$MYDOMAIN
+	[ $? -ne 0 ] && exit 1
+
+	sed "s/_MYDOMAIN_/$MYDOMAIN/g" -i /etc/bind/zones/$MYDOMAIN
+	[ $? -ne 0 ] && exit 1
+
+	sed "s/_MYIP_/$MYIP/g" -i /etc/bind/zones/$MYDOMAIN
+	[ $? -ne 0 ] && exit 1
+
+done
 
 rndc reconfig
 
 sleep 2
 
-echo ""
-dig test.$MYDOMAIN TXT +short @8.8.8.8
+for MYDOMAIN in $MYDOMAINS
+do
+	echo ""
+	echo "dig test.$MYDOMAIN TXT +short @8.8.8.8"
+	dig test.$MYDOMAIN TXT +short @8.8.8.8
+done
 echo ""
