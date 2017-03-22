9569fcc3ebfad36c748db25964e97caeae893448
Maciej Andzinski <m.andzinski@gmail.com>
2017-03-22 16:19:42 +0100

    Poprawki

--- autoconf/autoconf.sh
+++ autoconf/autoconf.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 MYIP=`ifconfig eth0 | perl -ne 'print $1 if m/inet addr:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/'`
 MYHOSTNAME=`hostname`
 MYDOMAINS=`dig ${MYHOSTNAME}.szkolenie.dnssec.pl txt +short | sort | tr -d '"' | tr "\n" " "`
@@ -14,58 +16,31 @@ case "$choice" in
 esac
 
 echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
-[ $? -ne 0 ] && exit 1
 
 apt-get update
-[ $? -ne 0 ] && exit 1
-
 apt-get install bind9 --yes
-[ $? -ne 0 ] && exit 1
-
 apt-get install vim --yes
-[ $? -ne 0 ] && exit 1
 
 cp named.conf.options /etc/bind/named.conf.options
-[ $? -ne 0 ] && exit 1
-
 echo 'include "/etc/bind/named.conf.zones";' >> /etc/bind/named.conf
-[ $? -ne 0 ] && exit 1
-
-
 touch /etc/bind/named.conf.zones
-[ $? -ne 0 ] && exit 1
-
 chgrp bind /etc/bind/named.conf.zones 
-[ $? -ne 0 ] && exit 1
-
 
 mkdir /etc/bind/zones
-[ $? -ne 0 ] && exit 1
 
 for MYDOMAIN in $MYDOMAINS
 do
 	cat zone-conf-tpl >> /etc/bind/named.conf.zones
-	[ $? -ne 0 ] && exit 1
-
 	sed "s/_MYDOMAIN_/$MYDOMAIN/g" -i /etc/bind/named.conf.zones
-	[ $? -ne 0 ] && exit 1
 
 	cp zone-tpl /etc/bind/zones/$MYDOMAIN
-	[ $? -ne 0 ] && exit 1
-
 	sed "s/_MYDOMAIN_/$MYDOMAIN/g" -i /etc/bind/zones/$MYDOMAIN
-	[ $? -ne 0 ] && exit 1
-
 	sed "s/_MYIP_/$MYIP/g" -i /etc/bind/zones/$MYDOMAIN
-	[ $? -ne 0 ] && exit 1
-
 done
 
 chown -R bind:bind /etc/bind/zones
-[ $? -ne 0 ] && exit 1
 
 service bind9 restart
-[ $? -ne 0 ] && exit 1
 
 sleep 2
 
